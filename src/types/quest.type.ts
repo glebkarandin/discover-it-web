19ad8bae06d305c7d1e8a61902cfe62c968a20e0
gkarandin <gkarandin@gmail.com>
2022-05-17 10:27:29 +0300

    Added question type

--- src/types/quest.type.ts
+++ src/types/quest.type.ts
@@ -1,10 +1,20 @@
 export interface IQuestBase {
   title: string,
   description: string
+  questionsRef: string
 }
 export interface IQuest extends IQuestBase{
   id: string,
 }
 
+export interface IQuestion {
+  questionText: string
+  questionImage?: string
+  answerText?: string
+  answerImage?: string
+  coords: string
+}
+
 export type QuestList = Array<IQuest>;
+export type QuestionList = Array<IQuestion>;
 
