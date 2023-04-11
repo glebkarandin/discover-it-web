79a67ff583e29ec4ef4459ea24010aee88ca3b4a
glebkarandin <gkarandin@gmail.com>
2023-04-11 15:41:40 +0300

    Add database connect

--- dit-backend/src/app.module.ts
+++ dit-backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+        envFilePath: '.env'
+      }),
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'dit-postgres',
+        port: 5432,
+        username: "glebka",
+        password: "password",
+        database: "ditDB",
+        entities: [],
+        synchronize: true,
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
